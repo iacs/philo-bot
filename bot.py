4ff81d4ba5bc9c02812a30c064363036c5dbc13f
Iago Mosquera <flackon@gmail.com>
2020-11-03 16:39:44 +0100

    Logging support

--- bot.py
+++ bot.py
@@ -1,12 +1,34 @@
 import os
+import logging
+import logging.handlers
+
 import discord
 
 from dotenv import load_dotenv
 
+LOGPATH = 'logs/philobot.log'
+
 
 class Philobot(discord.Client):
+    try:
+        log = logging.getLogger('philobot')
+        log.setLevel(logging.INFO)
+        fmtr = logging.Formatter(
+            '{asctime} - [{levelname}] {name}: {message} ({lineno})', style='{')
+        trfh = logging.handlers.TimedRotatingFileHandler(LOGPATH, 'W0', 1)
+        shd = logging.StreamHandler()
+        trfh.setFormatter(fmtr)
+        log.addHandler(trfh)
+        log.addHandler(shd)
+    except FileNotFoundError as err:
+        print(f'No file error: {err}. Create /logs directory')
+
+    def __init__(self):
+        super().__init__()
+
     async def on_ready(self):
-        print(f'{self.user} Raring to go!')
+        msg = f'{self.user} Raring to go!'
+        self.log.info(msg)
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -15,6 +37,9 @@ class Philobot(discord.Client):
         if message.content == '!rules':
             response = 'rules text coming soon'
             await message.channel.send(response)
+
+    async def on_disconnect(self):
+        self.log.info('disconnected')
 
 
 def main():
